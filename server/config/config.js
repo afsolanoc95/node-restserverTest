615ad5a5c7a31f07b43ef8f9d2b65849ef95aee1
afsolanoc95 <afsolanoc95@gmail.com>
2021-02-01 22:12:45 -0500

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ if(process.env.NODE_ENV==`dev`){
     urlDB=`mongodb://localhost:27017/coffee`
 }else{
    // urlDB=`mongodb+srv://admin:H2Z0lpjFjhtFzMz6@cluster0.ftxpi.mongodb.net/coffee?retryWrites=true&w=majority`
-    urlDB=process.env.URLDB;
+    urlDB=DB_URL;
 }
 //inventada
 process.env.URLDB =urlDB;
